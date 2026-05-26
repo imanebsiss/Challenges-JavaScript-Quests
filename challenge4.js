67c930cdcb18746351ee348e728bc01c5d277c98
imanebsiss <imane.bsiss@gmail.com>
2026-05-26 20:16:59 +0200

    Challenge 4 : compétition de gymnastique

---
+++ challenge4.js
@@ -0,0 +1,30 @@
+/*
+Données de test :
+Score Dauphins : 96, 108, 89 / Score Koalas : 88, 91, 110
+Score Dauphins : 97, 112, 101 / Score Koalas : 109, 95, 123
+Score Dauphins :97, 112, 101 / Score Koalas : 109, 95, 106
+*/
+
+
+let scoreDauphines = [97, 112, 101];        
+let scorKoalas = [109, 95, 123];
+
+let avrgDauphines = (97+ 112+ 101) / 3 ;          
+let avrgKoalas = (109 + 95 + 123) / 3 ;              
+
+console.log("Dauphines : " + avrgDauphines);
+console.log("Koalas : " + avrgKoalas);
+
+
+if ((avrgDauphines >= 100 ) && (avrgDauphines > avrgKoalas)){
+    console.log("L'équipe gagnate est : Dauphines");
+}
+else if ((avrgKoalas >= 100 ) && (avrgKoalas > avrgDauphines)) {
+    console.log("L'équipe gagnate est : Les Koalas");
+}
+else if ((avrgDauphines >= 100) && (avrgKoalas >= 100) && (avrgDauphines === avrgKoalas)){
+    console.log("Egalité des deux équipes!");
+}
+else {
+    console.log("Aucune équipe n'a gagné!");
+}
