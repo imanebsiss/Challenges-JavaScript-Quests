644e2c4b73d2b096d1d7711a9b3dd3845ea15b34
imanebsiss <imane.bsiss@gmail.com>
2026-05-26 20:46:09 +0200

    Challenge5 : tip calculator

---
+++ challenge5.js
@@ -0,0 +1,19 @@
+
+
+/*
+Données de test :
+Valeur de la note : 275, 40 et 430
+*/
+
+
+
+let invoice = 430;
+
+if ((invoice <= 300) && (invoice >= 50)) {
+    let tip = (invoice * 15) / 100;
+}
+else {
+    tip = (invoice * 20) / 100;
+}
+let totalValue = invoice + tip;
+console.log("La note était de " + invoice + ", le pourboire de " + tip + " et la valeur totale était de " + totalValue);
